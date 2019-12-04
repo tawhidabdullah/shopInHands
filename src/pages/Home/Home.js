8670483a68eeccdeefa8340ed54c408da23eb97b
tawhidabdullah <tawhidabdullah22@gmail.com>
2019-12-04 23:57:36 +0600

    added some cool stuffs

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -61,6 +61,499 @@ class Home extends Component {
             <FilterBar />
             <ProductList />
           </div> */}
+
+
+
+      <section className="product-slider-section">
+        <div
+          className="row"
+          style={{
+            borderBottom: '1px solid #ddd',
+          }}
+        >
+          <div className="col-md-6">
+            <h2 style={{fontWeight: 'bold', fontSize: '17px'}}>Computers</h2>
+          </div>
+          <div className="col-md-6">
+            {/* <div className="filter-taq-text">
+              <h3> 
+                Category
+              </h3>
+              <h3> 
+                Category
+              </h3>
+              <h3> 
+                Category
+              </h3>
+              <h3> 
+                Category
+              </h3>
+              <h3> 
+                Category
+              </h3>
+              <h3> 
+                Category
+              </h3>
+            </div> */}
+          </div>
+        </div>
+        <div className="row" style={{paddingTop: '20px'}}>
+
+        <div class="col-md-3">
+              <div className="product-card">
+              <div class="product-top">
+                <img src={require('./banner-3.jpg')} alt="product image" />
+                <div class="overlay-right">
+                  <button 
+                    type="button"
+                    class="btn btn-secondary"
+                    title="Quick Shop"
+                  >
+                    <i class="fa fa-eye"></i>
+                  </button>
+                  <button
+                    type="button"
+                    class="btn btn-secondary"
+                    title="Add To Wishlist"
+                  >
+                    <i class="fa fa-heart-o"></i>
+                  </button>
+                  <button
+                    type="button"
+                    class="btn btn-secondary"
+                    title="Add To Cart"
+                  >
+                    <i class="fa fa-shopping-cart"></i>
+                  </button>
+                </div>
+              </div>
+              <div class="product-bottom text-center">
+  
+              <div className='cart-btn'>
+              <button className='primary-btn'>
+                Add to Cart
+              </button>
+              </div>
+  
+                <div className='ratingsandtitle'>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star-half-o"></i>
+  
+                <h3 className='product-bottom-title'>Product Name</h3>
+                </div>
+                <h5 className='product-bottom-price'>$50.00</h5>
+              </div>
+              </div>
+            </div>
+
+
+            <div class="col-md-3">
+              <div className="product-card">
+              <div class="product-top">
+                <img src={require('./banner-3.jpg')} alt="product image" />
+                <div class="overlay-right">
+                  <button
+                    type="button"
+                    class="btn btn-secondary"
+                    title="Quick Shop"
+                  >
+                    <i class="fa fa-eye"></i>
+                  </button>
+                  <button
+                    type="button"
+                    class="btn btn-secondary"
+                    title="Add To Wishlist"
+                  >
+                    <i class="fa fa-heart-o"></i>
+                  </button>
+                  <button
+                    type="button"
+                    class="btn btn-secondary"
+                    title="Add To Cart"
+                  >
+                    <i class="fa fa-shopping-cart"></i>
+                  </button>
+                </div>
+              </div>
+              <div class="product-bottom text-center">
+  
+              <div className='cart-btn'>
+              <button className='primary-btn'>
+                Add to Cart
+              </button>
+              </div>
+  
+                <div className='ratingsandtitle'>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star-half-o"></i>
+  
+                <h3 className='product-bottom-title'>Product Name</h3>
+                </div>
+                <h5 className='product-bottom-price'>$50.00</h5>
+              </div>
+              </div>
+            </div>
+
+
+            <div class="col-md-3">
+              <div className="product-card">
+              <div class="product-top">
+                <img src={require('./banner-3.jpg')} alt="product image" />
+                <div class="overlay-right">
+                  <button
+                    type="button"
+                    class="btn btn-secondary"
+                    title="Quick Shop"
+                  >
+                    <i class="fa fa-eye"></i>
+                  </button>
+                  <button
+                    type="button"
+                    class="btn btn-secondary"
+                    title="Add To Wishlist"
+                  >
+                    <i class="fa fa-heart-o"></i>
+                  </button>
+                  <button
+                    type="button"
+                    class="btn btn-secondary"
+                    title="Add To Cart"
+                  >
+                    <i class="fa fa-shopping-cart"></i>
+                  </button>
+                </div>
+              </div>
+              <div class="product-bottom text-center">
+  
+              <div className='cart-btn'>
+              <button className='primary-btn'>
+                Add to Cart
+              </button>
+              </div>
+  
+                <div className='ratingsandtitle'>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star-half-o"></i>
+  
+                <h3 className='product-bottom-title'>Product Name</h3>
+                </div>
+                <h5 className='product-bottom-price'>$50.00</h5>
+              </div>
+              </div>
+            </div>
+
+
+            <div class="col-md-3">
+              <div className="product-card">
+              <div class="product-top">
+                <img src={require('./banner-3.jpg')} alt="product image" />
+                <div class="overlay-right">
+                  <button
+                    type="button"
+                    class="btn btn-secondary"
+                    title="Quick Shop"
+                  >
+                    <i class="fa fa-eye"></i>
+                  </button>
+                  <button
+                    type="button"
+                    class="btn btn-secondary"
+                    title="Add To Wishlist"
+                  >
+                    <i class="fa fa-heart-o"></i>
+                  </button>
+                  <button
+                    type="button"
+                    class="btn btn-secondary"
+                    title="Add To Cart"
+                  >
+                    <i class="fa fa-shopping-cart"></i>
+                  </button>
+                </div>
+              </div>
+              <div class="product-bottom text-center">
+  
+              <div className='cart-btn'>
+              <button className='primary-btn'>
+                Add to Cart
+              </button>
+              </div>
+  
+                <div className='ratingsandtitle'>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star-half-o"></i>
+  
+                <h3 className='product-bottom-title'>Product Name</h3>
+                </div>
+                <h5 className='product-bottom-price'>$50.00</h5>
+              </div>
+              </div>
+            </div>
+
+
+
+
+        </div>
+      </section>
+
+
+
+
+      <section className="product-slider-section">
+        <div
+          className="row"
+          style={{
+            borderBottom: '1px solid #ddd',
+          }}
+        >
+          <div className="col-md-6">
+            <h2 style={{fontWeight: 'bold', fontSize: '17px'}}>Computers</h2>
+          </div>
+          <div className="col-md-6">
+            {/* <div className="filter-taq-text">
+              <h3> 
+                Category
+              </h3>
+              <h3> 
+                Category
+              </h3>
+              <h3> 
+                Category
+              </h3>
+              <h3> 
+                Category
+              </h3>
+              <h3> 
+                Category
+              </h3>
+              <h3> 
+                Category
+              </h3>
+            </div> */}
+          </div>
+        </div>
+        <div className="row" style={{paddingTop: '20px'}}>
+
+        <div class="col-md-3">
+              <div className="product-card">
+              <div class="product-top">
+                <img src={require('./banner-3.jpg')} alt="product image" />
+                <div class="overlay-right">
+                  <button 
+                    type="button"
+                    class="btn btn-secondary"
+                    title="Quick Shop"
+                  >
+                    <i class="fa fa-eye"></i>
+                  </button>
+                  <button
+                    type="button"
+                    class="btn btn-secondary"
+                    title="Add To Wishlist"
+                  >
+                    <i class="fa fa-heart-o"></i>
+                  </button>
+                  <button
+                    type="button"
+                    class="btn btn-secondary"
+                    title="Add To Cart"
+                  >
+                    <i class="fa fa-shopping-cart"></i>
+                  </button>
+                </div>
+              </div>
+              <div class="product-bottom text-center">
+  
+              <div className='cart-btn'>
+              <button className='primary-btn'>
+                Add to Cart
+              </button>
+              </div>
+  
+                <div className='ratingsandtitle'>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star-half-o"></i>
+  
+                <h3 className='product-bottom-title'>Product Name</h3>
+                </div>
+                <h5 className='product-bottom-price'>$50.00</h5>
+              </div>
+              </div>
+            </div>
+
+
+            <div class="col-md-3">
+              <div className="product-card">
+              <div class="product-top">
+                <img src={require('./banner-3.jpg')} alt="product image" />
+                <div class="overlay-right">
+                  <button
+                    type="button"
+                    class="btn btn-secondary"
+                    title="Quick Shop"
+                  >
+                    <i class="fa fa-eye"></i>
+                  </button>
+                  <button
+                    type="button"
+                    class="btn btn-secondary"
+                    title="Add To Wishlist"
+                  >
+                    <i class="fa fa-heart-o"></i>
+                  </button>
+                  <button
+                    type="button"
+                    class="btn btn-secondary"
+                    title="Add To Cart"
+                  >
+                    <i class="fa fa-shopping-cart"></i>
+                  </button>
+                </div>
+              </div>
+              <div class="product-bottom text-center">
+  
+              <div className='cart-btn'>
+              <button className='primary-btn'>
+                Add to Cart
+              </button>
+              </div>
+  
+                <div className='ratingsandtitle'>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star-half-o"></i>
+  
+                <h3 className='product-bottom-title'>Product Name</h3>
+                </div>
+                <h5 className='product-bottom-price'>$50.00</h5>
+              </div>
+              </div>
+            </div>
+
+
+            <div class="col-md-3">
+              <div className="product-card">
+              <div class="product-top">
+                <img src={require('./banner-3.jpg')} alt="product image" />
+                <div class="overlay-right">
+                  <button
+                    type="button"
+                    class="btn btn-secondary"
+                    title="Quick Shop"
+                  >
+                    <i class="fa fa-eye"></i>
+                  </button>
+                  <button
+                    type="button"
+                    class="btn btn-secondary"
+                    title="Add To Wishlist"
+                  >
+                    <i class="fa fa-heart-o"></i>
+                  </button>
+                  <button
+                    type="button"
+                    class="btn btn-secondary"
+                    title="Add To Cart"
+                  >
+                    <i class="fa fa-shopping-cart"></i>
+                  </button>
+                </div>
+              </div>
+              <div class="product-bottom text-center">
+  
+              <div className='cart-btn'>
+              <button className='primary-btn'>
+                Add to Cart
+              </button>
+              </div>
+  
+                <div className='ratingsandtitle'>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star-half-o"></i>
+  
+                <h3 className='product-bottom-title'>Product Name</h3>
+                </div>
+                <h5 className='product-bottom-price'>$50.00</h5>
+              </div>
+              </div>
+            </div>
+
+
+            <div class="col-md-3">
+              <div className="product-card">
+              <div class="product-top">
+                <img src={require('./banner-3.jpg')} alt="product image" />
+                <div class="overlay-right">
+                  <button
+                    type="button"
+                    class="btn btn-secondary"
+                    title="Quick Shop"
+                  >
+                    <i class="fa fa-eye"></i>
+                  </button>
+                  <button
+                    type="button"
+                    class="btn btn-secondary"
+                    title="Add To Wishlist"
+                  >
+                    <i class="fa fa-heart-o"></i>
+                  </button>
+                  <button
+                    type="button"
+                    class="btn btn-secondary"
+                    title="Add To Cart"
+                  >
+                    <i class="fa fa-shopping-cart"></i>
+                  </button>
+                </div>
+              </div>
+              <div class="product-bottom text-center">
+  
+              <div className='cart-btn'>
+              <button className='primary-btn'>
+                Add to Cart
+              </button>
+              </div>
+  
+                <div className='ratingsandtitle'>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star-half-o"></i>
+  
+                <h3 className='product-bottom-title'>Product Name</h3>
+                </div>
+                <h5 className='product-bottom-price'>$50.00</h5>
+              </div>
+              </div>
+            </div>
+
+
+
+
+        </div>
+      </section>
       </React.Fragment>
     );
   }
