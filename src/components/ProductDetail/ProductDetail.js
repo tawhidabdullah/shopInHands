0fcea907528763e852fb6488886a4737d250b4a9
tawhidabdullah <tawhidabdullah22@gmail.com>
2019-12-09 03:10:31 +0600

    added

--- src/components/ProductDetail/ProductDetail.js
+++ src/components/ProductDetail/ProductDetail.js
@@ -13,38 +13,31 @@ const ProductDetail = props => {
   return (
     <aside className="col-sm-7">
       <article className="card-body p-5">
-        <h3 className="title mb-3">{name}</h3>
+        <h3 className="title mt-3 mb-3" style={{marginLeft: '-12px'}}>{name}</h3>
+        <div class="row">
+        <h1><i class="fa fa-dollar" aria-hidden="true"></i> {price}</h1>
+        &nbsp; &nbsp;
+        <h3><del>799</del></h3>
+        &nbsp; &nbsp;
+        <h2 class="text-success">30% off</h2>
+      </div>
 
-        <p className="price-detail-wrap">
+      <div class="row">
+        <h3 class="text-warning"><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i> <i class="fa fa-star-half-o" aria-hidden="true"></i><i class="fa fa-star-o" aria-hidden="true"></i></h3>
+        &nbsp; &nbsp;
+        <h5>1200 star rating  and 250 reviews</h5>
+      </div>
+      
+
+        {/* <p className="price-detail-wrap">
           <span className="price h3 text-primary">
             <span className="currency">$</span>
             <span className="num">{price}</span>
           </span>
-        </p>
-        <div class="product-rating">
-          <ul>
-            <li class="fa fa-fw fa-lg fa-star" />
-            <li class="fa fa-fw fa-lg fa-star" />
-            <li class="fa fa-fw fa-lg fa-star" />
-            <li class="fa fa-fw fa-lg fa-star" />
-            <li class="fa fa-fw fa-lg fa-star-half" />
-          </ul>
-          <span class="product-reviews">
-            {/* <a href="#">{reviews.length} Reviews</a> */}
-          </span>
-        </div>
-        <dl className="item-property">
-          <dt>Description</dt>
-          <dd>
-            <p className="text-capitalize">{desc}</p>
-          </dd>
-        </dl>
-        <dl className="param param-feature">
-          <dt>Category</dt>
-          {/* <dd className="text-capitalize">{category}</dd> */}
-        </dl>
+        </p> */}
+      
+      
 
-        <hr />
         <hr />
         <button className="btn btn-lg btn-outline-primary mr-4 ">
           Buy Now
