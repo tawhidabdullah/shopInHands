1a3033108a56c35c32bb52518d6b83be0fbc9aff
tawhidabdullah <tawhidabdullah22@gmail.com>
2019-12-10 11:51:09 +0600

    added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -57,7 +57,7 @@ class Header extends Component {
         <div className='navbar'>
           <div className='navbar-center'>
           
-            <span className='nav-icon' style={{
+            <span className='nav-icon nav-menu' style={{
               display: 'block',
               fontSize: '18px',
               padding: '10px 25px',
