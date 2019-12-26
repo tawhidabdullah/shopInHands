c6e9bfa20cf88783eaaf0b835b0972948b484c9f
tawhidabdullah <tawhidabdullah22@gmail.com>
2019-12-26 21:59:07 +0600

    push build files

---
+++ build/precache-manifest.4db3a647a5f1a78339227da40922802c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "191d7c044291ddd16c76",
+    "url": "/static/js/main.191d7c04.chunk.js"
+  },
+  {
+    "revision": "fc82e0222287220ccca3",
+    "url": "/static/js/2.fc82e022.chunk.js"
+  },
+  {
+    "revision": "191d7c044291ddd16c76",
+    "url": "/static/css/main.2598545c.chunk.css"
+  },
+  {
+    "revision": "fc82e0222287220ccca3",
+    "url": "/static/css/2.3139b100.chunk.css"
+  },
+  {
+    "revision": "a21a21c0c798e76662e91f1c5b417893",
+    "url": "/index.html"
+  }
+];
